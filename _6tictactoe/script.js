6b3561b2a91503af00de10747a87d74a0686fe01
GpGardner <58527818+GpGardner@users.noreply.github.com>
2020-01-31 08:01:24 -0500

    Add files via upload

--- _6tictactoe/script.js
+++ _6tictactoe/script.js
@@ -11,49 +11,56 @@ var part9 = document.querySelector(".part9");
 var button = document.querySelector(".test");
 
 
+
+
 //TODO : ADD WIN LOOP THAT STOPS AND RESETS -- PARTIALLY DONE
 //       KEEP SCORE p1 and p2
 //       ADD X's and O's
 //       BEAUTIFY
 //       REFACTOR
+//       ADD ABILITY TO CHOOSE COLOR OF EACH PLAYER?
 
 
 // Color Picker Game Loop
-for (let i = 0; i < box.length; i++) {
-box[i].addEventListener("click", function() {
-  if (this.classList.contains("white")) {
-    this.classList.remove("white");
-    this.classList.add("green");
-  } else if (this.classList.contains("green")) {
-    this.classList.remove("green");
-    this.classList.add("red");
-  } else if(this.classList.contains("red")) {
-    this.classList.remove("red");
-    this.classList.add("white");
+
+  for (let i = 0; i < box.length; i++) {
+  box[i].addEventListener("click", function() {
+    if (this.classList.contains("white")) {
+      this.classList.remove("white");
+      this.classList.add("green");
+    } else if (this.classList.contains("green")) {
+      this.classList.remove("green");
+      this.classList.add("red");
+    } else if(this.classList.contains("red")) {
+      this.classList.remove("red");
+      this.classList.add("white");
+    }
+  });
   }
-});
-}
+
 
 
 // red win conditions
 
 document.addEventListener("click", function() {
 if (part1.classList.contains("red") && part4.classList.contains("red") && part7.classList.contains("red")) {
-  console.log("youwin")
+  console.log("youwin");
+  alert("game over");  
   } if (part1.classList.contains("red") && part2.classList.contains("red") && part3.classList.contains("red")) {
-    console.log("youwin")
+    console.log("youwin");
+
     } if (part1.classList.contains("red") && part5.classList.contains("red") && part9.classList.contains("red")) {
-      console.log("youwin")
+      console.log("youwin");
       } if (part4.classList.contains("red") && part5.classList.contains("red") && part6.classList.contains("red")) {
-        console.log("youwin")
+        console.log("youwin");
         } if (part7.classList.contains("red") && part8.classList.contains("red") && part9.classList.contains("red")) {
-          console.log("youwin")
+          console.log("youwin");
           } if (part2.classList.contains("red") && part5.classList.contains("red") && part8.classList.contains("red")) {
-            console.log("youwin")
+            console.log("youwin");
             } if (part3.classList.contains("red") && part6.classList.contains("red") && part9.classList.contains("red")) {
-              console.log("youwin")
+              console.log("youwin");
               }  if (part3.classList.contains("red") && part5.classList.contains("red") && part7.classList.contains("red")) {
-                console.log("youwin")
+                console.log("youwin");
               }
               });
 
@@ -61,21 +68,21 @@ if (part1.classList.contains("red") && part4.classList.contains("red") && part7.
 
 document.addEventListener("click", function() {
   if (part1.classList.contains("green") && part4.classList.contains("green") && part7.classList.contains("green")) {
-    console.log("youwin")
+    console.log("youwin");
     } if (part1.classList.contains("green") && part2.classList.contains("green") && part3.classList.contains("green")) {
-      console.log("youwin")
+      console.log("youwin");
       } if (part1.classList.contains("green") && part5.classList.contains("green") && part9.classList.contains("green")) {
-        console.log("youwin")
+        console.log("youwin");
         } if (part4.classList.contains("green") && part5.classList.contains("green") && part6.classList.contains("green")) {
-          console.log("youwin")
+          console.log("youwin");
           } if (part7.classList.contains("green") && part8.classList.contains("green") && part9.classList.contains("green")) {
-            console.log("youwin")
+            console.log("youwin");
             } if (part2.classList.contains("green") && part5.classList.contains("green") && part8.classList.contains("green")) {
-              console.log("youwin")
+              console.log("youwin");
               } if (part3.classList.contains("green") && part6.classList.contains("green") && part9.classList.contains("green")) {
-                console.log("youwin")
+                console.log("youwin");
                 }  if (part3.classList.contains("green") && part5.classList.contains("green") && part7.classList.contains("green")) {
-                  console.log("youwin")
+                  console.log("youwin");
                 }
                 });
 
